f17058e03aa01d7a461b9683a893a0e8b25e3616
David Aprilio <david1.aprilio4@gmail.com>
2023-08-14 15:26:18 +0700

    fixing example pairing with QR

--- tests/connect_to_wa.ts
+++ tests/connect_to_wa.ts
@@ -1,73 +1,37 @@
 import Whatsapp, { Client } from "../src/Whatsapp";
-// import fs from "fs";
-// import {
-    // MessageType,
-    // getChatId,
-    // generateWAMessage,
-    // makeInMemoryStore
-    // getDevice,
-// } from '@whiskeysockets/baileys'
+import { question } from "../src/utils";
 
-
-const wa: Whatsapp = new Whatsapp('david-14A', {
+const wa: Whatsapp = new Whatsapp('ez-wa', {
     browser: Client.Opera,
-    useStore: true
 });
 
 (async () => {
-    console.log('Starting Socket');
-    console.time('connect');
-    await wa.startSock()
-    console.log('Socket Started');
-    
-    await wa.waitSockConnected()
-    console.timeEnd('connect');
-
-    console.log('Sock Connected');
-    wa.on('msg.incoming', async () => {
-        // const textMsg = message.message?.conversation || message.message?.extendedTextMessage?.text
-        // if (textMsg === 'ping') {
-        //     const msg = wa.createMessage()
-        //     msg.text('Pong!')
-        //     msg.reply(message)
-        // }
-
-        // if (message.key.remoteJid!.includes('@g.')) {
-        //     await wa.sock.groupMetadata(message.key.remoteJid!).then((data) => {
-        //         console.log('Group: ', data.subject)
-        //         console.log(data.id);
-        //     })
-        // }
-
-        // console.log(message)
-
-        // // save to file
-        // const filename = 'test'
-        // fs.writeFileSync(
-        //     `./${filename}.json`, 
-        //     JSON.stringify(message, undefined, 4)
-        // )
+    console.log('EZ-Whatsapp (Pairing With QR)');
+    wa.on('sock.connecting', () => {
+        console.log('Connecting...');
     })
 
-    // console.log('Device', getDevice(''));
-    // console.log('Getting Data');
-    // const data = await wa.sock.
-
-    // const ss: MessageType = 'audioMessage'
-
-    // console.log('Data:', wa.getStore?.messages['sas']);
     wa.on('sock.disconnected', (reason) => {
         console.log('Koneksi Terputus', reason);
-    })    
-    // setTimeout(async () => {
-    //     await wa.logout()
-    // }, 35_000);
+    })
 
-    await wa.sendMessageWithTyping('628884966841', {
-        text: 'test'
-    }, undefined, 5_000)
+    await wa.startSock()
+    console.log('Socket Started');
+    await wa.waitSockConnected()
 
-    console.log('terkirim');
-    
+    console.log(wa.info.id, 'Connected as', wa.info.jid, wa.info.pushName);
+    wa.on('msg.incoming', async ({message}) => {
+        const msg = wa.createMessage()
+        msg.text('ping!')
+        msg.reply(message)
+    })
 
+    while (true) {
+        console.log('\nSend Message');
+        const phone = await question('Enter your phone: ');
+        const text = await question('Enter your message: ');
+        const msg = wa.createMessage()
+        msg.text(text)
+        await msg.send(phone).then((result) => console.log(result))
+    }
 })()
